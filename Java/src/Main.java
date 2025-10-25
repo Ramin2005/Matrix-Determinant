10fc1df5316d06af727df1d47e154cddd6f887b0
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-10-25 04:12:10 +0330

    update tests

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -9,7 +9,6 @@ import with_counter.Laplace;
 public class Main {
 
     public static void main(String[] args) {
-        
     }
 
     public static void test1() {
@@ -50,7 +49,7 @@ public class Main {
 
     public static void test2() {
         try {
-            for (int n = 1; n <= 13; n++) {
+            for (int n = 1; n <= 12; n++) {
                 String path = "./test2/output_" + n + ".txt";
                 File file = new File(path);
                 Formatter out = new Formatter(file);
@@ -61,7 +60,7 @@ public class Main {
                 out.format("Matrix:%n");
                 for (int i = 0; i < n; i++) {
                     for (int j = 0; j < n; j++) {
-                        out.format("%f ", matrix[i][j]);
+                        out.format("%2.0f ", matrix[i][j]);
                     }
                     out.format("%n");
                 }
@@ -89,17 +88,36 @@ public class Main {
                 countLaplace = (long) temp2[1];
                 timeLaplace = System.nanoTime() - startTime2;
 
+                int qubed = n * n * n;
+
                 //LU
                 out.format("LU Decomposition:%n");
                 out.format("Determinant: %f%n", detLU);
                 out.format("Operation Count: %d%n", countLU);
+                double operationCubedRatio = (double) qubed / countLU;
+                out.format("Operation Count LU / n^3: %f%n", operationCubedRatio);
                 out.format("Time (ns): %d%n%n", timeLU);
+
+                //fac n
+                int factorial = 1;
+                for (int i = 2; i <= n; i++) {
+                    factorial *= i;
+                }
 
                 //Laplace
                 out.format("Laplace Expansion:%n");
                 out.format("Determinant: %f%n", detLaplace);
                 out.format("Operation Count: %d%n", countLaplace);
+                double operationFactorialRatio = (double) factorial / countLaplace;
+                out.format("Operation Count Laplace / n!: %f%n", operationFactorialRatio);
                 out.format("Time (ns): %d%n%n", timeLaplace);
+
+                double operationRatio = (double) countLaplace / countLU;
+                out.format("Operation Count Laplace / LU: %f%n", operationRatio);
+
+                double timeRatio = (double) timeLaplace / timeLU;
+                out.format("Time Laplace / LU: %f%n%n", timeRatio);
+
                 out.close();
 
                 System.out.printf(" - Laplace done %fms%n", timeLaplace / 1e6);
